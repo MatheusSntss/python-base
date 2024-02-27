f4d6018662306dfc37e613d331d0b3613c67e1f5
Matheus Santos <matheus.snts.pvh@gmail.com>
2024-02-27 11:45:15 -0400

    Implementando Hello Word multi Linguas

--- hello.py
+++ hello.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
+"""
+Hello  Word Multi Linguas
+
+Como usar:
+Tenha o LANG devidamente configurado
+Ex: LANG=pt_br
+
+Modo de uso:
+	python3 hello.py
+	./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Matheus Santos"
+__license__ = "Unlicense"
+
+import os
  
-print ("Hello Word!") 
+current_linguage = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello Word!" 
+if current_linguage == "pt_BR":
+	msg = "Ola mundo!"
+
+print (msg)
