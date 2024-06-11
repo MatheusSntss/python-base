4e5c4cd7d688ed963dfb416dec851bb0bafc325a
Matheus Santos <matheus.snts.pvh@gmail.com>
2024-06-11 16:31:18 -0400

    Implementei hello words Multi Linguas

--- hello.py
+++ hello.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
-# SHEBANG /\
+"""Hello word Multi Linguas
+
+Dependendo da lingua configuradano ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar: 
+Tenha a variavel LANG devidamente configurada ex:
+
+    LANGUAGE=en_US
+Execucao:
+    python3 hello.py
+ou
+    ./hello.py
+"""
+
+__version__="0.0.1"
+__author__="Matheus Santos"
+__license__= "unlicense"
+# Dunder
+
+import os
+#Ler variavel de ambiente - 
+
 # Este programa imprime hello word
 
-print ("Hello,Word")
+
+current_language = os.getenv("LANG")[:5]
+# snake case
+msg = "Hello,Word"
+
+if current_language == "pt_BR":
+    msg = "Ola, mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, mondo!"
+
+print(msg)
