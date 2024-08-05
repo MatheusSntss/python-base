79c0b9578d820c759ea455829ad69a16f2f28d20
Matheus Santos <matheus.snts.pvh@gmail.com>
2024-08-05 16:00:10 -0400

    adicionado Log

--- hello.py
+++ hello.py
@@ -20,6 +20,18 @@ __version__="0.1.1"
 __author__="Matheus Santos"
 __license__= "unlicense"
 # Dunder
+import logging 
+
+log = logging.Logger("hello.py", logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+fmt = logging.Formatter(
+    '%(asctime)s, %(name)s, %(levelname)s, l:%(lineno)d f:%(filename)s: %(message)s '
+)
+ch.setFormatter(fmt)
+log.addHandler(ch)
 
 import os
 #Ler variavel de ambiente - 
@@ -28,15 +40,23 @@ import sys
 arguments = {"lang":None,"count":1}
 for arg in sys.argv[1:]:
     # TODO: Tratar valueError
-     key,value = arg.split("=")
-     key = key.lstrip("-").strip() 
-     if key not in arguments:
+    try:
+        key,value = arg.split("=")
+
+    except ValueError as e:
+        #TODO:Logging
+        log.error("you need '=', you passed %s, try --key=value: %s ",arg,str(e))
+ 
+        sys.exit(1)
+        
+    key = key.lstrip("-").strip() 
+    if key not in arguments:
         print (f"Invalid Option ´{key}´")
         sys.exit()
-     arguments[key] = value
-
+    arguments[key] = value
      
 current_language = arguments["lang"]
+
 if current_language is None:    
     current_language = os.getenv("LANG")
     # TODO : usar repetição
@@ -44,6 +64,8 @@ if current_language is None:
         current_language = input("Choose language:")
 
 current_language = current_language[:5]
+
+
 msg = { 
     "en_US": "Hello, World",
     "pt_BR": "Olá Mundo",
@@ -51,6 +73,12 @@ msg = {
     "es_SP": "Hola, Mundo"
  }
 
+# message = msg.get(current_language,msg["en_US"])
+try:
+    message = msg[current_language]
+except KeyError:
+    print(f"Language is invalid, try {msg}")
+    sys.exit(1)
 
 #Cli --
-print(msg[current_language] * int(arguments["count"]))
+print(message * int(arguments["count"]))
