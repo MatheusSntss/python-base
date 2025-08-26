5d25674e3508e7b2fdf39c40ff84d6f6f1eb889d
MatheusSntss <matheus.snts.pvh@gmail.com>
2025-08-26 14:08:29 +0000

    Usando manipulacao de arquivo no envio de emails

--- email.py
+++ email.py
@@ -3,28 +3,30 @@
 """
 Imprime mensagem de um e-mail
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
+import os
+import sys
 
-email_tmpl = """
-Olá, %(nome)s
 
-Tem interesse em comprar %(produto)s?
+argumentos = sys.argv[1:]
+if not argumentos:
+    print("Informe o  nome do arquivo de emails")
+    sys.exit(1)
 
-Este produto é ótimo para resolver %(texto)s
-
-Clique agora em %(link)s
-
-Apenas %(quantidade)d disponiveis!
-
-Preço promocional R$%(preco).2f
-"""
-clientes = ["Maria","Marta","Joao"]
-
-for cliente in clientes:
+filename = argumentos[0]
+file_tmpl = argumentos[1]
+path = os.curdir
+filepath = os.path.join(path,filename) #email.txt
+templatepath = os.path.join(path,file_tmpl) #email_tmpl.txt
+for line in open(filepath):
+    nome,email=line.split(",")
+    
+    print(f"Enviando e-mail para:{email}")
+    print()
     print(
-        email_tmpl
-        % {
-            "nome":cliente,
+        open(templatepath).read()
+            %{
+            "nome":nome,
             "produto":"Caneta",
             "texto": "Escrever muito bem",
             "link": "http//canetaslegais.com",
@@ -32,3 +34,4 @@ for cliente in clientes:
             "preco": 2.25
         }
     )
+    print("-" * 50)
