4c85a3fb2c643750215a694e691c07d29f50ec63
MatheusSntss <matheus.snts.pvh@gmail.com>
2025-09-16 13:52:42 +0000

    Arrumando bug do while

--- 04_input/infixcalc.py
+++ 04_input/infixcalc.py
@@ -36,56 +36,63 @@ logger.addHandler(console_handler)
 
 __version__ = "0.1.1"
 
+argumentos = sys.argv[1:]
 
 operacoes = {
     "sum": lambda a, b: a + b,
     "sub": lambda a, b: a - b,
     "mul": lambda a, b: a * b,
     "div": lambda a, b: a / b,
-}
-
-argumentos = sys.argv[1:]
-
-if not argumentos:
-    operacao = input("Operação:")
-    n1 = input("n1:")
-    n2 = input("n2:")
-    argumentos = [operacao, n1, n2]
-elif len(argumentos) != 3:
-    print("Quantidade de argumentos inválido")
-    print("ex `num 5 5`")
-    sys.exit(1)
-
-operacao, *nums = argumentos
-
-
-
-if operacao not in operacoes:
-    print("A operação não existe")
-    print(operacoes.keys())
-    sys.exit(1)
+    }
     
-numeros_validos = []
-for num in nums:
-    if not num.replace(".","").isdigit():
-        print(f"Numéro invalido {num}")
-    if "." in num:
-        num = float(num)
-    else:
-        num = int(num)
-    numeros_validos.append(num)
-n1,n2 = numeros_validos
-
-resultado = operacoes[operacao](n1,n2)
-
-print(f"O resultado é: {resultado}")
-
 path = os.curdir
 filepath = os.path.join(path,"infixcalc.log")
 timestamp = datetime.now().isoformat()
 user = os.getenv("USER","anonymous")
-try:
-    with open(filepath,"a") as file_:
-        file_.write(f"{timestamp} - {user} - {operacao},{n1},{n2} = {resultado}\n")
-except PermissionError as e:
-    logger.error("Você não tem permissão para criar o arquivo, %s",str(e))
+
+while True:
+
+
+
+    if not argumentos:
+        operacao = input("Operação:")
+        n1 = input("n1:")
+        n2 = input("n2:")
+        argumentos = [operacao, n1, n2]
+    elif len(argumentos) != 3:
+        print("Quantidade de argumentos inválido")
+        print("ex `num 5 5`")
+        sys.exit(1)
+
+    operacao, *nums = argumentos
+
+    if operacao not in operacoes:
+        print("A operação não existe")
+        print(operacoes.keys())
+        sys.exit(1)
+        
+    numeros_validos = []
+    for num in nums:
+        if not num.replace(".","").isdigit():
+            print(f"Numéro invalido {num}")
+        if "." in num:
+            num = float(num)
+        else:
+            num = int(num)
+        numeros_validos.append(num)
+    n1,n2 = numeros_validos
+
+    resultado = operacoes[operacao](n1,n2)
+
+    print(f"O resultado é: {resultado}")
+
+    try:
+        with open(filepath,"a") as log:
+            log.write(f"{timestamp} - {user} - {operacao},{n1},{n2} = {resultado}\n")
+    except PermissionError as e:
+        logger.error("Você não tem permissão para criar o arquivo, %s",str(e))
+        sys.exit(1)
+        
+    argumentos = None
+    if input("Pressione Enter para continuar ou qualquer tecla para sair"):
+        break
