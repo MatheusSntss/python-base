8b5d49699ef4bc424bc2cb680a2625c1d3d1fc5e
MatheusSntss <matheus.snts.pvh@gmail.com>
2025-09-16 13:57:49 +0000

    reorganizando

--- 03_tuplas_listas/escola_v1_com_lista.py
+++ 03_tuplas_listas/escola_v1_com_lista.py
@@ -6,7 +6,7 @@ Imprimir a lista de crianças agrupadas por sala
 que frequenta cada uma das atividades.
 
 """
-__version__ = "0.1.1"
+__version__ = "0.1.0"
 
 #Dados
 sala1 = ["Erik","Maia","Gustavo","Manoel","Sofia","Joana"]
@@ -15,18 +15,22 @@ sala2 = ["Joao","Antonio","Carlos","Maria"]
 atividade_ingles = ["Erik", "Maia","Joana","Carlos","Antonio"]
 atividade_musica = ["Erik", "Carlos","Maia"]
 
-atividades = {
-    "Inglês":atividade_ingles, 
-    "Música":atividade_musica,
-    }
+atividades = [
+    ("Inglês",atividade_ingles), 
+    ("Música",atividade_musica),
+    ]
 
-
+atividade_sala1 = []
+atividade_sala2 = []
 #Lista alunos em cada atividade por sala
-for nome_atividade, aluno_na_atividade in atividades.items():
+for nome_atividade, atividade in atividades:
     print(f"Alunos da atividade {nome_atividade}\n")
     print("-" * 30)
-    atividade_sala1 = set(sala1) & set(aluno_na_atividade)
-    atividade_sala2 = set(sala2).intersection(aluno_na_atividade)
+    for aluno in atividade:
+        if aluno in sala1:
+            atividade_sala1.append(aluno)
+        else:
+            atividade_sala2.append(aluno)
     print(f"Sala 1:{atividade_sala1}")
     print(f"Sala 2:{atividade_sala2}")
     print("-" * 30)
